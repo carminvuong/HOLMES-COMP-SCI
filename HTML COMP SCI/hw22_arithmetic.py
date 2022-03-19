2791631cd52d66bb5f1efdc24186f1200c384fa3
carminvuong <95779419+carminvuong@users.noreply.github.com>
2022-03-19 16:31:51 -0400

    add HTML COMP SCI

---
+++ HTML COMP SCI/hw22_arithmetic.py
@@ -0,0 +1,17 @@
+# Carmin Vuong
+# hw22: arithmetic
+# worked with no one
+# advised by no one
+
+# Task 3
+print(5/9*(32 - 32)) # 32 degrees F is 0 degrees C 
+
+print(5/9*(0 - 32)) # 33 degrees F is 5/9 degrees C or around 0.56 degrees C
+
+print(5/9*(-1 - 32)) # -1.1 degrees F is around -18.33 degrees C 
+
+
+# Task 4
+print((((1 + 5 ** 0.5) / 2)**7 - ((1 - 5 ** 0.5) / 2)**7) / 5**(0.5)) # 7th element in the Fibonacci sequence
+
+print((((1 + 5 ** 0.5) / 2)**10 - ((1 - 5 ** 0.5) / 2)**10) / 5**(0.5)) # 10th element in the Fibonacci sequence

b28cd0d2cc487d34bf457b92ab5ef7541d01354b
carminvuong <95779419+carminvuong@users.noreply.github.com>
2022-04-05 18:46:17 -0400

    update hw31

--- HTML AND PYTHON COMP SCI/hw31_taxBracket.py
+++ HTML AND PYTHON COMP SCI/hw31_taxBracket.py
@@ -1,11 +1,15 @@
 # Carmin Vuong
 # hw31: recover & finish if
-# worked with Anthony Chen
+# worked with Joseph Mei
 # advised by no one
 
 
 # ----------------- taxBracket function ------------------------
+
+# Pre-condition: only accepts non-negative integers as inputs
 def taxBracket(taxableIncome):
+# Post-condition: returns the tax rate based on your input 
+
     if taxableIncome <= 9875:
         return 10/100
     elif taxableIncome <= 40125:
@@ -25,9 +29,9 @@ def taxBracket(taxableIncome):
 
 
 # ---------------- test cases ------------------------
-print(taxBracket(9000) == 10/100)   # non edge case
-print(taxBracket(9876) == 12/100)   # edge case
-print(taxBracket(50888) == 22/100)   # non edge case
-print(taxBracket(163300) == 24/100)   # edge case
-print(taxBracket(186000) == 32/100)   # non edge case
-print(taxBracket(518401) == 37/100)   # edge case
+print(abs(taxBracket(6000) - 10/100) < 0.001)   # non edge case
+print(abs(taxBracket(9876) - 12/100) < 0.001)   # edge case
+print(abs(taxBracket(50888) - 22/100) < 0.001)   # non edge case
+print(abs(taxBracket(163300) - 24/100) < 0.001)   # edge case
+print(abs(taxBracket(200000) - 32/100) < 0.001)   # non edge case
+print(abs(taxBracket(518401) - 37/100) < 0.001)   # edge case

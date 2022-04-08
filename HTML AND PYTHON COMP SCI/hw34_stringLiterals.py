a72cf96f29dc15608830f7ad0bcbac8bfaf62631
carminvuong <95779419+carminvuong@users.noreply.github.com>
2022-04-08 19:57:07 -0400

    add hw34

---
+++ HTML AND PYTHON COMP SCI/hw34_stringLiterals.py
@@ -0,0 +1,35 @@
+# Carmin Vuong
+# hw34: string intro
+# worked with no one
+# advised by no one
+
+def printBracketed( problemID, printMe):
+    print( problemID, '[' + printMe + ']')
+    print( '-----------------------')
+
+
+# --------- task 1D -----------
+printBracketed( 0, 'Omit needless words')
+
+
+printBracketed( 1, '"The road to hell is paved with adverbs."')
+
+
+printBracketed( 2, '''"The adjective hasn't been built that can pull a weak or inaccurate noun out of a tight place."''')
+
+
+printBracketed( 3,
+'''<a href="https://nationalpoetryday.co.uk/poem/my-heart-leaps-up/">
+  William Wordsworth
+  </a>''')
+
+
+printBracketed( 4, '''If you find that a worthwhile task
+requires much more work to do really well
+than you ever dreamed, you're probably
+doing it correctly.\n
+If you make it look easy,
+you're definitely doing it correctly.''')
+
+
+printBracketed( 5, '')
